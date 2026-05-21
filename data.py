2bc0c76e8d34ba2df9812f3f9cfb627643930a79
arieljms <arieljames695@gmail.com>
2026-05-21 17:35:59 +0700

    add data and least-cost method

---
+++ data.py
@@ -0,0 +1,31 @@
+# Data input untuk optimasi biaya distribusi semen
+# Studi Kasus: TB. Medang Indah (2023)
+
+supply = [600, 450, 250]  
+
+agents = [
+    "Agen 1 (Gading Serpong)",
+    "Agen 2 (BSD)",
+    "Agen 3 (Alam Sutra)"
+]
+
+demand = [300, 100, 200, 300, 50, 100, 50, 50, 100, 50]
+
+destinations = [
+    "Curug Sangereng (CS)",
+    "Kelapa Dua (KD)",
+    "Bojong Nangka (BN)",
+    "Pagedangan (PG)",
+    "Karang Tengah (KT)",
+    "Cibogo (CB)",
+    "Cisauk (CSK)",
+    "Pakulonan Barat (PB)",
+    "Cihuni (CH)",
+    "Cijantra (CJ)"
+]
+
+cost = [
+    [39.1, 40.2, 37.6, 40.6, 43.1, 40.0, 41.0, 40.6, 41.6, 37.6],
+    [43.7, 44.1, 45.1, 42.1, 51.8, 42.6, 41.1, 43.1, 40.1, 42.7],
+    [42.1, 40.6, 43.1, 44.5, 43.3, 45.3, 46.3, 41.4, 42.3, 45.8],  
+]
